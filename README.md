6c5f179062279d4077d8f696ca038a5d544758b9
Nomi Yu <nomiyu134@gmail.com>
2025-07-13 21:43:49 -0700

    website
    wrapping up
    
    merge
    
    slight edits
    
    init commit
    
    slight edits
    
    Update README.md
    
    cleanup
    
    cleaning
    
    good retrieval
    
    add disclaimer

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# gencad3d.github.io
+# GenCAD-3D
+
+This website is inpired from the repository for the [Nerfies website](https://nerfies.github.io) and [GenCAD website](https://github.com/gencad/gencad.github.io)
+
+If you find GenCAD-3D useful for your work please cite:
+```
+@article{yu2025gencad3dreverseengineercadprogram,
+            title={GenCAD=3D: CAD Program Generation using Multimodal Latent Space Alignment and Synthetic Dataset Balancing}, 
+            author={Nomi Yu, Md Ferdous Alam, A John Hart, Faez Ahmed},
+            year={2025},
+            journal={JMD},
+            eprint={TBA},
+            archivePrefix={arXiv},
+            url={TBA}, 
+        }
+```
+
+# Website License
+<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
---
+++ static/js/util.js
@@ -0,0 +1,108 @@
+// Method 1: Web Components (Custom Elements)
+class LoadGeo extends HTMLElement {
+    connectedCallback() {
+        const title = this.getAttribute('title') || 'Default Title';
+        const modality = this.getAttribute('modality');
+        const dataid = this.getAttribute('dataid');
+        const src = this.getAttribute('src');
+        const task = this.getAttribute('task');
+        const show_cad_program = this.getAttribute('show_cad_program');
+
+        if (show_cad_program === 'true') {
+            const modelSrc = src ? `static/${task}/${modality}/${dataid}/${src}.png` : 'default-model.png';
+            
+            this.innerHTML = `
+                <p style="text-align: center">${title}</p>
+                <img src="${modelSrc}" alt="${title}">
+            `;
+        } else {
+            const modelSrc = src ? `static/${task}/${modality}/${dataid}/${src}.gltf` : 'default-model.gltf';
+            
+            this.innerHTML = `
+                <p style="text-align: center">${title}</p>
+                <model-viewer 
+                    alt="${title}"
+                    src="${modelSrc}" 
+                    shadow-intensity="1" 
+                    camera-controls 
+                    touch-action="pan-y"
+                    camera-orbit="45deg 75deg"
+                    style="width: auto;"
+                >
+                </model-viewer>
+            `;
+        }
+
+    }
+}
+customElements.define('load-geo', LoadGeo);
+
+// Method 1: Web Components (Custom Elements)
+class LoadFolder extends HTMLElement {
+    connectedCallback() {
+        const title = this.getAttribute('title') || 'Default Title';
+        const modality = this.getAttribute('modality');
+        const dataid = this.getAttribute('dataid');
+        const task = this.getAttribute('task');
+        const show_cad_program = this.getAttribute('show_cad_program');
+        
+        this.innerHTML = `
+            <div class="generation_examp">
+                <div>
+                    <load-geo title="Ground Truth" src="expected" modality="${modality}" dataid="${dataid}" task="${task}" show_cad_program="${show_cad_program}"</load-geo>
+                </div>
+                <div>
+                    <load-geo title="Input" src="input" modality="${modality}" dataid="${dataid}" task="${task}" show_cad_program=false </load-geo>
+                </div>
+                <div>
+                    <load-geo title="${(task == "generated") ? "sample" : "top-1"}" src="0" modality="${modality}" dataid="${dataid}" task="${task}" show_cad_program="${show_cad_program}" </load-geo>
+                </div>
+                <div>
+                    <load-geo title="${(task == "generated") ? "sample" : "top-2"}" src="1" modality="${modality}" dataid="${dataid}" task="${task}" show_cad_program="${show_cad_program}" </load-geo>
+                </div>
+                <div>
+                    <load-geo title="${(task == "generated") ? "sample" : "top-3"}" src="2" modality="${modality}" dataid="${dataid}" task="${task}" show_cad_program="${show_cad_program}" </load-geo>
+                </div>
+                <div>
+                    <load-geo title="${(task == "generated") ? "sample" : "top-4"}" src="3" modality="${modality}" dataid="${dataid}" task="${task}" show_cad_program="${show_cad_program}" </load-geo>
+                </div>
+                <div>
+                    <load-geo title="${(task == "generated") ? "sample" : "top-5"}" src="4" modality="${modality}" dataid="${dataid}" task="${task}" show_cad_program="${show_cad_program}" </load-geo>
+                </div>
+            </div>
+        `;
+    }
+}
+customElements.define('load-folder', LoadFolder);
+
+class LoadAug extends HTMLElement {
+    connectedCallback() {
+        const title = this.getAttribute('title') || 'Default Title';
+        const modality = "aug";
+        const dataid = this.getAttribute('dataid');
+        const task = "augmentations";
+        const show_cad_program = this.getAttribute('show_cad_program');
+        
+        this.innerHTML = `
+            <div class="generation_examp">
+                <div>
+                    <load-geo title="Original" src="orig" modality="${modality}" dataid="${dataid}" task="${task}" show_cad_program="${show_cad_program}" </load-geo>
+                </div>
+                <div>
+                    <load-geo title="Augmentation" src="0" modality="${modality}" dataid="${dataid}" task="${task}" show_cad_program="${show_cad_program}" </load-geo>
+                </div>
+                <div>
+                    <load-geo title="Augmentation" src="1" modality="${modality}" dataid="${dataid}" task="${task}" show_cad_program="${show_cad_program}" </load-geo>
+                </div>
+                <div>
+                    <load-geo title="Augmentation" src="2" modality="${modality}" dataid="${dataid}" task="${task}" show_cad_program="${show_cad_program}" </load-geo>
+                </div>
+                <div>
+                    <load-geo title="Augmentation" src="3" modality="${modality}" dataid="${dataid}" task="${task}" show_cad_program="${show_cad_program}" </load-geo>
+                </div>
+            </div>
+        `;
+    }
+}
+customElements.define('load-aug', LoadAug);
+
